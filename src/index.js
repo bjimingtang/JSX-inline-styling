a9c95e8d31ae687edd7940d5014c712a247030f4
bjimingtang <brendan.jiming.tang@gmail.com>
2020-12-17 01:49:32 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const color_red = { color: "red" };
+
+ReactDOM.render(
+  <h1 style={color_red}>Hello World!</h1>,
+  document.getElementById("root")
+);
